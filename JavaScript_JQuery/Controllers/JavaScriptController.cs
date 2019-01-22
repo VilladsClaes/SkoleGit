14d7b7091db6918de85a2a7985dbc923d4622ba8
Villads Claes Olesen <vill0281@videndjurs.net>
2019-01-22 20:23:29 +0100

    Initial Commit

---
+++ JavaScript_JQuery/Controllers/JavaScriptController.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JavaS.Controllers
+{
+    public class JavaScriptController : Controller
+    {
+        // GET: JavaScript
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Opg01()
+        {
+            return View();
+        }
+        public ActionResult Opg02()
+        {
+            return View();
+        }
+        public ActionResult Opg03()
+        {
+            return View();
+        }
+        public ActionResult Opg04()
+        {
+            return View();
+        }
+        public ActionResult Opg05()
+        {
+            return View();
+
+        }
+        public ActionResult Opg06()
+        {
+            return View();
+
+        }
+        public ActionResult Opg07()
+        {
+            return View();
+
+        }
+        public ActionResult Opg08()
+        {
+            return View();
+
+        }
+        public ActionResult Opg09()
+        {
+            return View();
+
+        }
+        public ActionResult Opg10()
+        {
+            return View();
+
+        }
+        public ActionResult Opg11()
+        {
+            return View();
+
+        }
+        public ActionResult Opg12()
+        {
+            return View();
+
+        }
+        public ActionResult Opg13()
+        {
+            return View();
+
+        }
+        public ActionResult Opg14()
+        {
+            return View();
+
+        }
+        public ActionResult Opg15()
+        {
+            return View();
+
+        }
+        public ActionResult Opg16()
+        {
+            return View();
+
+        }
+        public ActionResult Opg17()
+        {
+            return View();
+
+        }
+        public ActionResult fotogalleri()
+        {
+            return View();
+
+        }
+
+    }
+}
---
+++ JavaScript_JQuery/Controllers/jQueryController.cs
@@ -0,0 +1,69 @@
+﻿using JavaS.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JavaS.Controllers
+{
+    public class jQueryController : Controller
+    {
+       
+        
+        public ActionResult opg01()
+        {
+            return View();
+        }
+        public ActionResult opg02()
+        {
+            return View();
+        }
+        public ActionResult opg03()
+        {
+            return View();
+        }
+        public ActionResult opg04()
+        {
+            return View();
+        }
+        public ActionResult opg05()
+        {
+            return View();
+        }
+        public ActionResult opg06()
+        {
+            return View();
+        }
+        public ActionResult opg07()
+        {
+            return View();
+        }
+        public ActionResult opg08()
+        {
+            return View();
+        }
+        public ActionResult opg09()
+        {
+            return View();
+
+        }
+        public ActionResult opg10()
+        {
+            return View();
+        }
+
+        PopopDatabaseEntities db = new PopopDatabaseEntities();
+        public ActionResult Popup()
+        {
+            //Instantiér en liste items og fyld den med indholdet fra databasen som en liste
+            List<Models.tblPopup> items = db.tblPopup.ToList();
+
+            return View(items);
+        }
+        public ActionResult slider()
+        {
+            return View();
+        }
+    }
+}
