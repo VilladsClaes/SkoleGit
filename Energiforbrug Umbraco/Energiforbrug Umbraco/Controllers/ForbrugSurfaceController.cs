74434c8a051bf1b4ffb21bc052a10c6adc0013ee
Villads Claes <villadsclaes@villadsclaes.dk>
2019-08-28 23:21:22 +0200

    lidt mere

---
+++ Energiforbrug Umbraco/Energiforbrug Umbraco/Controllers/ForbrugSurfaceController.cs
@@ -0,0 +1,34 @@
+﻿using Energiforbrug_Umbraco.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+
+namespace Energiforbrug_Umbraco.Controllers
+{
+    public class ForbrugSurfaceController : SurfaceController
+    {
+        // GET: ForbrugSurface
+        public ActionResult Index()
+        {
+            return PartialView("Forbrug", new Forbrugsting());
+        }
+
+        [HttpPost]
+        public ActionResult Forbrug(Forbrugsting model)
+        {
+
+            var NyNode = Services.ContentService.CreateContent(model.ForbrugskildeNavn, 1077, "forbrug", 0);
+            NyNode.SetValue("ForbrugskildeNavn", model.ForbrugskildeNavn);
+            NyNode.SetValue("AntalMinutterIBrug", model.AntalMinutterIBrug);
+            NyNode.SetValue("ForbrugMaengde", model.ForbrugMaengde);
+            NyNode.SetValue("Forbrugstype", model.Forbrugstype);
+            Services.ContentService.SaveAndPublishWithStatus(NyNode);
+
+
+            return CurrentUmbracoPage();
+        }
+    }
+}
---
+++ Energiforbrug Umbraco/Energiforbrug Umbraco/Models/Forbrugsting.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Energiforbrug_Umbraco.Models
+{
+    public class Forbrugsting
+    {
+        public string ForbrugskildeNavn { get; set; }
+        public int ForbrugMaengde { get; set; }
+
+        public string Forbrugstype { get; set; }
+
+        public string AntalMinutterIBrug  { get; set; }
+    }
+}
+
